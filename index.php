8e3e429b983768eb2441ca6e3bb219d2dd609162
Viktor Kovacs <kovvik@gmail.com>
2016-08-19 08:25:56 +0000

    changed url for cloudinary

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 $assets_url="http://my-awesome-site.local/assets/";
 
 $files = array (
-    "v1471533898/Free_logo_tcod2x.svg",
-    "v1471533898/tumblr_mvv8e1OQX51qj73e2o1_500_viaiep.gif",
-    "v1471533899/SH00020-Multiple-Square-Logo-Template_wvwy9x.png",
-    "v1471594573/SampleVideo_1280x720_1mb_gp31qc.mp4"
+    "image/upload/v1471533898/Free_logo_tcod2x.svg",
+    "image/upload/v1471533898/tumblr_mvv8e1OQX51qj73e2o1_500_viaiep.gif",
+    "image/upload/v1471533899/SH00020-Multiple-Square-Logo-Template_wvwy9x.png",
+    "video/upload/v1471594573/SampleVideo_1280x720_1mb_gp31qc.mp4"
 );
 
 $image = $assets_url.$files[rand(0, 2)];
