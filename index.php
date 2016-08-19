9460f2a921632f134e2cb1f91de1ab7c7b65bafe
Viktor Kovacs <kovvik@gmail.com>
2016-08-19 08:19:46 +0000

    corrected typo in files array

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $assets_url="http://my-awesome-site.local/assets/";
 $files = array (
     "v1471533898/Free_logo_tcod2x.svg",
     "v1471533898/tumblr_mvv8e1OQX51qj73e2o1_500_viaiep.gif",
-    "v1471533899/SH00020-Multiple-Square-Logo-Template_wvwy9x.png"
+    "v1471533899/SH00020-Multiple-Square-Logo-Template_wvwy9x.png",
     "v1471594573/SampleVideo_1280x720_1mb_gp31qc.mp4");
 
 $image = $assets_url.$files[rand(0, 2)];
