f802dc0c22bb8abaffc235aaca6e5d1aff7cd586
Viktor Kovacs <kovvik@gmail.com>
2016-08-19 08:17:12 +0000

    video addes

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ $files = array (
     "v1471533898/Free_logo_tcod2x.svg",
     "v1471533898/tumblr_mvv8e1OQX51qj73e2o1_500_viaiep.gif",
     "v1471533899/SH00020-Multiple-Square-Logo-Template_wvwy9x.png"
-    );
+    "v1471594573/SampleVideo_1280x720_1mb_gp31qc.mp4");
 
-$content = $assets_url.$files[rand(0, 2)];
+$image = $assets_url.$files[rand(0, 2)];
+$video = $assets_url.$files[3];
 include "templates/main.tpl";
--- templates/main.tpl
+++ templates/main.tpl
@@ -4,7 +4,11 @@
 	</head>
 	<body>
 		<h1>My Awesome App</h1>
-		<img src="<?php echo $content ?>" />
+		<img src="<?php echo $image ?>" />
+		<hr>
+		<video height="240" width="320" controls>
+			<source src="<?php echo $video ?>" type="video/mp4">
+		</video>
 	</body>
 </html>
 
